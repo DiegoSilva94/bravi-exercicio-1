38ccb1306df58b50672b8ac7b7844721783b1891
Diego da Silva Oliveira <dsilvaoliveira94@gmail.com>
2024-06-04 00:58:28 -0300

    adicionando README

---
+++ README.md
@@ -0,0 +1,1 @@
+Para testar o programa basta executar o execute.sh, ele usará o docker para executar uma camada de testes.
--- execute.sh
+++ execute.sh
@@ -7,18 +7,9 @@ if [ ! -d "./vendor" ]; then
       laravelsail/php83-composer:latest \
       composer install
 fi
-if [ $1 = "teste" ]; then
-  docker run --rm \
-      -u "$(id -u):$(id -g)" \
-      -v "$(pwd):/var/www/html" \
-      -w /var/www/html \
-      laravelsail/php83-composer:latest \
-      ./vendor/bin/phpunit teste.php
-else
-  docker run --rm \
-      -u "$(id -u):$(id -g)" \
-      -v "$(pwd):/var/www/html" \
-      -w /var/www/html \
-      laravelsail/php83-composer:latest \
-      php -f index.php
-fi
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php83-composer:latest \
+    ./vendor/bin/phpunit teste.php
