e3593ac6c1d38da2ac9c47d6bda60cf3d03f86bf
Diego da Silva Oliveira <dsilvaoliveira94@gmail.com>
2024-06-04 00:56:33 -0300

    removendo validação de regex teste

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 /**
- * Nessa tentativa eu usei um metodo bem simples para testar mas possui um problema com falso positivo.
- * @param string $parametros
- * @return bool
- */
-function validaEntradaDeParametrosComRegexBasico(string $parametros): bool
-{
-    return preg_replace('/\(.*\)|\{.*\}|\[.*\]/', '', $parametros) === '';
-}
-
-/**
  * Então para não perder muito tempo na criação de um regex funcional com todas as validações de colchetes dentro de
  * colchetes vamos para uma tecnica mais pratica.
  * @param string $parametros
--- teste.php
+++ teste.php
@@ -3,30 +3,6 @@ use PHPUnit\Framework\TestCase;
 
 final class FuncaoTeste extends TestCase
 {
-    public function testEntradaDeParametrosValidasComRegex(): void
-    {
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('()'));
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('{}'));
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('[]'));
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('(){}'));
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('()[]'));
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('[]{}'));
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('(){}[]'));
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('[{()}]{}[]'));
-    }
-
-    public function testEntradaDeParametrosInvalidasComRegex(): void
-    {
-        $this->assertFalse(validaEntradaDeParametrosComRegexBasico('(){[]'));
-        $this->assertFalse(validaEntradaDeParametrosComRegexBasico('([{)]'));
-        $this->assertFalse(validaEntradaDeParametrosComRegexBasico('{([})]'));
-    }
-
-    public function testEntradaDeParametrosFalsoPositivoComRegex(): void
-    {
-        $this->assertTrue(validaEntradaDeParametrosComRegexBasico('{([}')); // o regex basico não detecta abertura sem fechamento.
-    }
-
     public function testEntradaDeParametrosValidas(): void
     {
         $this->assertTrue(validaEntradaDeParametros('()'));
